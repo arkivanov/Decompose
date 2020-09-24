4ce47c1848db50931ba1c8c56d58bc806a31cf98
Arkadii Ivanov <arkann1985@gmail.com>
2020-09-24 18:21:43 +0100

    Remove Value.render extension

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/ValueExt.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/ValueExt.kt
@@ -1,17 +1,5 @@
 package com.arkivanov.decompose.value
 
-import com.arkivanov.decompose.lifecycle.Lifecycle
-import com.arkivanov.decompose.lifecycle.doOnStart
-import com.arkivanov.decompose.lifecycle.doOnStop
 import kotlin.reflect.KProperty
 
 operator fun <T : Any> Value<T>.getValue(thisRef: Any?, property: KProperty<*>): T = value
-
-fun <T : Any> Value<T>.render(lifecycle: Lifecycle, render: (T) -> Unit) {
-    lifecycle.doOnStart {
-        render(value)
-        subscribe(render)
-    }
-
-    lifecycle.doOnStop { unsubscribe(render) }
-}
