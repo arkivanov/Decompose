0c3041cbbcfb3eedb5aa0493687fe6e294684251
Arkadii Ivanov <arkann1985@gmail.com>
2020-09-24 16:22:50 +0100

    Add crossfade transition for sample Todo Android app

--- sample/todo/root/src/androidMain/kotlin/com/arkivanov/todo/root/TodoRootComposable.kt
+++ sample/todo/root/src/androidMain/kotlin/com/arkivanov/todo/root/TodoRootComposable.kt
@@ -1,5 +1,6 @@
 package com.arkivanov.todo.root
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.invoke
 import com.arkivanov.todo.edit.invoke
@@ -7,10 +8,12 @@ import com.arkivanov.todo.main.invoke
 
 @Composable
 operator fun TodoRoot.Model.invoke() {
-    child { child ->
-        when (child) {
-            is TodoRoot.Child.Main -> child.model()
-            is TodoRoot.Child.Edit -> child.model()
+    child {
+        Crossfade(current = it) { child ->
+            when (child) {
+                is TodoRoot.Child.Main -> child.model()
+                is TodoRoot.Child.Edit -> child.model()
+            }
         }
     }
 }
