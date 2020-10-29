b16e2f7bc1f8d248ab25357a080146f0ff5ae5d1
Arkadii Ivanov <arkann1985@gmail.com>
2020-10-29 21:43:34 +0000

    Add Value.observe with lifecycle extension function

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/ValueExt.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/ValueExt.kt
@@ -1,5 +1,39 @@
 package com.arkivanov.decompose.value
 
+import com.arkivanov.decompose.lifecycle.Lifecycle
+import com.arkivanov.decompose.lifecycle.subscribe
 import kotlin.reflect.KProperty
 
 operator fun <T : Any> Value<T>.getValue(thisRef: Any?, property: KProperty<*>): T = value
+
+fun <T : Any> Value<T>.observe(
+    lifecycle: Lifecycle,
+    mode: ObserveLifecycleMode = ObserveLifecycleMode.START_STOP,
+    observer: ValueObserver<T>
+) {
+    when (mode) {
+        ObserveLifecycleMode.CREATE_DESTROY ->
+            lifecycle.subscribe(
+                onCreate = { subscribe(observer) },
+                onDestroy = { unsubscribe(observer) }
+            )
+
+        ObserveLifecycleMode.START_STOP ->
+            lifecycle.subscribe(
+                onStart = { subscribe(observer) },
+                onStop = { unsubscribe(observer) }
+            )
+
+        ObserveLifecycleMode.RESUME_PAUSE ->
+            lifecycle.subscribe(
+                onResume = { subscribe(observer) },
+                onPause = { unsubscribe(observer) }
+            )
+    }.let {}
+}
+
+enum class ObserveLifecycleMode {
+    CREATE_DESTROY,
+    START_STOP,
+    RESUME_PAUSE
+}
