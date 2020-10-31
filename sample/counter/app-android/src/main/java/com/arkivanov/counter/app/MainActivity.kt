6ee22eba23242aebd126dd0178933af5d20d0e76
Aleksandr E <a.efimenko72@gmail.com>
2020-10-31 13:40:10 +0500

    Upgrade Compose to alpha06

--- sample/counter/app-android/src/main/java/com/arkivanov/counter/app/MainActivity.kt
+++ sample/counter/app-android/src/main/java/com/arkivanov/counter/app/MainActivity.kt
@@ -2,11 +2,11 @@ package com.arkivanov.counter.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Box
-import androidx.compose.foundation.ContentGravity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import com.arkivanov.counter.app.ui.ComposeAppTheme
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         setContent {
             ComposeAppTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Box(gravity = ContentGravity.Center, modifier = Modifier.fillMaxSize()) {
+                    Box(modifier = Modifier.fillMaxSize(), alignment = Alignment.Center) {
                         rootComponent(::CounterRootContainer).model()
                     }
                 }
