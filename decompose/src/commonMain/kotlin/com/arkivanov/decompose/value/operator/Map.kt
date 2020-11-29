784db86fd7dc449c7b72f4a7707f3cbf99ffee21
Arkadii Ivanov <arkann1985@gmail.com>
2020-11-29 14:22:53 +0000

    Fix Value.map returns old value when not subscribed"

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/operator/Map.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/operator/Map.kt
@@ -1,6 +1,5 @@
 package com.arkivanov.decompose.value.operator
 
-import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.decompose.value.ValueObserver
 
@@ -10,29 +9,44 @@ private class MappedValue<T : Any, out R : Any>(
     private val upstream: Value<T>,
     private val mapper: (T) -> R
 ) : Value<R>() {
-    private val mutableValue = MutableValue(mapper(upstream.value))
-    private var upstreamObserver: ValueObserver<T>? = null
-    private val observers = mutableSetOf<ValueObserver<R>>()
+    private var lastUpstreamValue: T = upstream.value
+    private var lastDownstreamValue: R = mapper(lastUpstreamValue)
 
-    override val value: R get() = mutableValue.value
+    override val value: R
+        get() {
+            val upstreamValue = upstream.value
+            if (upstreamValue !== lastUpstreamValue) {
+                lastUpstreamValue = upstreamValue
+                lastDownstreamValue = mapper(upstreamValue)
+            }
+
+            return lastDownstreamValue
+        }
+
+    private var observers = emptySet<ValueObserver<R>>()
+    private val upstreamObserver: (T) -> Unit = ::onUpstreamValue
+
+    private fun onUpstreamValue(value: T) {
+        lastUpstreamValue = value
+        val mappedValue = mapper(value)
+        lastDownstreamValue = mappedValue
+        observers.forEach { it(mappedValue) }
+    }
 
     override fun subscribe(observer: ValueObserver<R>) {
         if (observers.isEmpty()) {
-            upstreamObserver = { mutableValue.value = mapper(it) }
-            upstream.subscribe(upstreamObserver!!)
+            upstream.subscribe(upstreamObserver)
         }
 
-        observers += observer
-        mutableValue.subscribe(observer)
+        this.observers += observer
+        observer(value)
     }
 
     override fun unsubscribe(observer: ValueObserver<R>) {
-        mutableValue.unsubscribe(observer)
-        observers -= observer
+        this.observers -= observer
 
         if (observers.isEmpty()) {
-            upstream.unsubscribe(upstreamObserver!!)
-            upstreamObserver = null
+            upstream.unsubscribe(upstreamObserver)
         }
     }
 }
