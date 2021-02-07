cf174a074db72c4000570d113ee63b5e187cb513
Arkadii Ivanov <arkann1985@gmail.com>
2021-02-07 22:14:38 +0000

    Update Kotlin to 1.4.30, JetBrains Compose to 0.3.0-build152, Jetpack Compose to 1.0.0-alpha12

--- extensions-compose-jetpack/src/main/java/com/arkivanov/decompose/extensions/compose/jetpack/RouterStateComposable.kt
+++ extensions-compose-jetpack/src/main/java/com/arkivanov/decompose/extensions/compose/jetpack/RouterStateComposable.kt
@@ -1,36 +1,34 @@
 package com.arkivanov.decompose.extensions.compose.jetpack
 
-import android.os.Parcelable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.savedinstancestate.ExperimentalRestorableStateHolder
-import androidx.compose.runtime.savedinstancestate.RestorableStateHolder
-import androidx.compose.runtime.savedinstancestate.rememberRestorableStateHolder
+import androidx.compose.runtime.saveable.SaveableStateHolder
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import com.arkivanov.decompose.RouterState
+import com.arkivanov.decompose.statekeeper.Parcelable
 import com.arkivanov.decompose.value.Value
 
 typealias ChildContent<C, T> = @Composable (child: T, configuration: C) -> Unit
 
 typealias ChildAnimation<C, T> = @Composable (child: T, configuration: C, ChildContent<C, T>) -> Unit
 
-@OptIn(ExperimentalRestorableStateHolder::class)
 @Composable
 fun <C : Parcelable, T : Any> Children(
     routerState: Value<RouterState<C, T>>,
     animation: ChildAnimation<C, T> = { child, configuration, childContent -> childContent(child, configuration) },
     content: ChildContent<C, T>
 ) {
-    val holder = key(routerState) { rememberRestorableStateHolder<C>() }
+    val holder = key(routerState) { rememberSaveableStateHolder() }
     val state by routerState.asState()
     val activeChild = state.activeChild
 
     holder.retainStates(state.getConfigurations())
 
     animation(activeChild.component, activeChild.configuration) { child, configuration ->
-        holder.RestorableStateProvider(configuration) {
+        holder.SaveableStateProvider(configuration) {
             content(child, configuration)
         }
     }
@@ -44,9 +42,8 @@ private fun <C : Parcelable> RouterState<C, *>.getConfigurations(): Set<C> {
     return set
 }
 
-@OptIn(ExperimentalRestorableStateHolder::class)
 @Composable
-private fun <T : Any> RestorableStateHolder<T>.retainStates(currentKeys: Set<T>) {
+private fun SaveableStateHolder.retainStates(currentKeys: Set<Any>) {
     val keys = remember(this) { Keys(currentKeys) }
 
     DisposableEffect(this, currentKeys) {
@@ -62,6 +59,6 @@ private fun <T : Any> RestorableStateHolder<T>.retainStates(currentKeys: Set<T>)
     }
 }
 
-private class Keys<T : Any>(
-    var set: Set<T>
+private class Keys(
+    var set: Set<Any>
 )
