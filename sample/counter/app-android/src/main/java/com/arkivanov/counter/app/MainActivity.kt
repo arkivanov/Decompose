ead50ae3a59e9008c33b4db7e5633d294384b856
Arkadii Ivanov <arkann1985@gmail.com>
2021-02-24 20:22:20 +0000

    Update Jetpack Compose to 1.0.0-beta01 and JetBrains Compose to 0.4.0-build168

--- sample/counter/app-android/src/main/java/com/arkivanov/counter/app/MainActivity.kt
+++ sample/counter/app-android/src/main/java/com/arkivanov/counter/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.arkivanov.counter.app
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +9,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.setContent
 import com.arkivanov.counter.app.ui.ComposeAppTheme
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.ExperimentalDecomposeApi
