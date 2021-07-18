d05a26b20e87965e783a2dc0f76b3d3c029fffb3
Arkadii Ivanov <arkann1985@gmail.com>
2021-07-18 11:01:31 +0100

    Added default value to RouterStack.backStack property

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/RouterState.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/RouterState.kt
@@ -2,5 +2,5 @@ package com.arkivanov.decompose
 
 data class RouterState<out C : Any, out T : Any>(
     val activeChild: Child.Created<C, T>,
-    val backStack: List<Child<C, T>>
+    val backStack: List<Child<C, T>> = emptyList()
 )
