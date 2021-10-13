761a119cd82ce53ce6043f54c9726c4bc1b9a8e9
Arkadii Ivanov <arkann1985@gmail.com>
2021-10-13 19:25:19 +0100

    Fix Android library publication artifact names

--- tools/check-publication/build.gradle.kts
+++ tools/check-publication/build.gradle.kts
@@ -9,7 +9,7 @@ setupMultiplatform {
 }
 
 repositories {
-    maven("https://oss.sonatype.org/service/local/staging/") {
+    maven("https://oss.sonatype.org/content/groups/staging/") {
         credentials {
             username = "arkivanov"
             password = System.getenv("SONATYPE_PASSWORD")
