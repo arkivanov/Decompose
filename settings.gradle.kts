28a97d1cf356c49dda866e21d5f1cef188927dda
Arkadii Ivanov <arkann1985@gmail.com>
2021-10-21 19:51:30 +0100

    Updated gradle-setup-plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.toString() == "com.arkivanov.gradle.setup") {
-                useModule("com.github.arkivanov:gradle-setup-plugin:44ca9f629c")
+                useModule("com.github.arkivanov:gradle-setup-plugin:2812f68eec")
             }
         }
     }
