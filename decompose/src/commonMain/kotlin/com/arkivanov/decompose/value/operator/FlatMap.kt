55b5c9598460b11339629ffabb16ea83dffc42e2
Thijs de Haan <thijsdehaan71@hotmail.com>
2022-05-06 12:52:41 +0200

    add FlattenedValue

---
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/operator/FlatMap.kt
@@ -0,0 +1,50 @@
+package com.arkivanov.decompose.value.operator
+
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.decompose.value.ValueObserver
+
+fun <T: Any> Value<Value<T>>.flatten() : Value<T> = FlattenedValue(this)
+
+private class FlattenedValue<out T : Any>(
+    private val upstream : Value<Value<T>>
+) : Value<T>(){
+    override val value: T
+        get() = upstream.value.value
+
+    private var observers = emptySet<ValueObserver<T>>()
+
+    private var valueT : Value<T> = upstream.value
+
+    private var valueObserver : ValueObserver<T> = ::notifyObservers
+    private var valueTObserver : ValueObserver<Value<T>> = ::changeValueT
+
+
+    fun notifyObservers(value : T){
+        observers.forEach { it(value) }
+    }
+
+    fun changeValueT(value: Value<T>){
+        if (valueT!=value){
+            valueT.unsubscribe(valueObserver)
+            value.subscribe(valueObserver)
+        }
+        valueT = value
+    }
+
+    override fun subscribe(observer: ValueObserver<T>) {
+        if (observers.isEmpty()){
+            upstream.subscribe(valueTObserver)
+        }
+
+        this.observers+=observer
+        observer(value)
+    }
+
+    override fun unsubscribe(observer: ValueObserver<T>) {
+        this.observers-=observer
+
+        if (observers.isEmpty()){
+            upstream.unsubscribe(valueTObserver)
+        }
+    }
+}
