9a6e4a7cd8a8a3afc6b13a17268c3453ba79f798
Arkadii Ivanov <arkann1985@gmail.com>
2022-05-31 21:31:39 +0100

    Added ensureNeverFrozen to MutableValueImpl

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/MutableValueBuilder.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/value/MutableValueBuilder.kt
@@ -1,11 +1,17 @@
 package com.arkivanov.decompose.value
 
+import com.arkivanov.decompose.ensureNeverFrozen
 import kotlin.properties.Delegates
 
 @Suppress("FunctionName") // Factory function
 fun <T : Any> MutableValue(initialValue: T): MutableValue<T> = MutableValueImpl(initialValue)
 
 private class MutableValueImpl<T : Any>(initialValue: T) : MutableValue<T>() {
+
+    init {
+        ensureNeverFrozen()
+    }
+
     private var observers = emptySet<ValueObserver<T>>()
 
     override var value: T by Delegates.observable(initialValue) { _, _, value ->
