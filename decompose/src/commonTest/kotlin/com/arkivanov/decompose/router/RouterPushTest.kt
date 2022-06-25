fa5ab4be20ad1c33dd839874befe4ff5eece11d5
Arkadii Ivanov <arkann1985@gmail.com>
2022-06-25 21:31:41 +0100

    Added onComplete callback for all navigation extensions

---
+++ decompose/src/commonTest/kotlin/com/arkivanov/decompose/router/RouterPushTest.kt
@@ -0,0 +1,28 @@
+package com.arkivanov.decompose.router
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+@Suppress("TestFunctionName")
+class RouterPushTest {
+
+    @Test
+    fun WHEN_push_THEN_pushed() {
+        val router = TestRouter(listOf(1, 2))
+
+        router.push(3)
+
+        assertEquals(listOf(1, 2, 3), router.stack)
+    }
+
+    @Test
+    fun WHEN_push_THEN_onComplete_called() {
+        val router = TestRouter(listOf(1, 2))
+        var isCalled = false
+
+        router.push(3) { isCalled = true }
+
+        assertTrue(isCalled)
+    }
+}
