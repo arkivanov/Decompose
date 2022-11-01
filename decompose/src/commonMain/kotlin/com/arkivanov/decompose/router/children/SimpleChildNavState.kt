460c6199ca20fd3cb36e1f3432574ef84ab84b50
Arkadii Ivanov <arkann1985@gmail.com>
2022-11-01 19:19:14 +0000

    Added SimpleNavigation and exposed SimpleChildNavState

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/router/children/SimpleChildNavState.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/router/children/SimpleChildNavState.kt
@@ -5,8 +5,8 @@ import com.arkivanov.decompose.ExperimentalDecomposeApi
 /**
  * A simple implementation of the [ChildNavState] interface.
  */
-@OptIn(ExperimentalDecomposeApi::class)
-internal data class SimpleChildNavState<out C : Any>(
+@ExperimentalDecomposeApi
+data class SimpleChildNavState<out C : Any>(
     override val configuration: C,
     override val status: ChildNavState.Status,
 ) : ChildNavState<C>
---
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/router/children/SimpleNavigation.kt
@@ -0,0 +1,26 @@
+package com.arkivanov.decompose.router.children
+
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.Relay
+
+/**
+ * A simple implementation of the [NavigationSource] interface.
+ * Broadcasts navigation events to every subscribed observer.
+ */
+@ExperimentalDecomposeApi
+class SimpleNavigation<T : Any> : NavigationSource<T> {
+
+    private val relay = Relay<T>()
+
+    override fun subscribe(observer: (T) -> Unit) {
+        relay.subscribe(observer)
+    }
+
+    override fun unsubscribe(observer: (T) -> Unit) {
+        relay.unsubscribe(observer)
+    }
+
+    fun navigate(event: T) {
+        relay.accept(event)
+    }
+}
