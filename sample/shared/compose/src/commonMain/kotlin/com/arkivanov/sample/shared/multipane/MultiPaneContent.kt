a7b4f14ad8a8710e76230d6145dff63ede39bf14
Arkadii Ivanov <arkann1985@gmail.com>
2023-01-29 19:09:10 +0000

    Use Generic Navigation in Multi-Pane sample

--- sample/shared/compose/src/commonMain/kotlin/com/arkivanov/sample/shared/multipane/MultiPaneContent.kt
+++ sample/shared/compose/src/commonMain/kotlin/com/arkivanov/sample/shared/multipane/MultiPaneContent.kt
@@ -3,56 +3,80 @@ package com.arkivanov.sample.shared.multipane
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.SaveableStateHolder
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
-import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
-import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+import com.arkivanov.decompose.Child
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
-import com.arkivanov.decompose.router.stack.ChildStack
-import com.arkivanov.decompose.value.Value
-import com.arkivanov.sample.shared.multipane.MultiPaneComponent.DetailsChild
-import com.arkivanov.sample.shared.multipane.MultiPaneComponent.ListChild
+import com.arkivanov.sample.shared.multipane.details.ArticleDetailsComponent
 import com.arkivanov.sample.shared.multipane.details.ArticleDetailsContent
+import com.arkivanov.sample.shared.multipane.list.ArticleListComponent
 import com.arkivanov.sample.shared.multipane.list.ArticleListContent
-
-private val MULTI_PANE_WIDTH_THRESHOLD = 800.dp
-private const val LIST_PANE_WEIGHT = 0.4F
-private const val DETAILS_PANE_WEIGHT = 0.6F
 
 @Composable
 internal fun MultiPaneContent(component: MultiPaneComponent, modifier: Modifier = Modifier) {
+    val children by component.children.subscribeAsState()
+    val listChild = children.listChild
+    val detailsChild = children.detailsChild
+
+    val saveableStateHolder = rememberSaveableStateHolder()
+
+    val listPane: @Composable (Child.Created<*, ArticleListComponent>) -> Unit =
+        remember {
+            movableContentOf { (config, component) ->
+                saveableStateHolder.SaveableStateProvider(key = config) {
+                    ArticleListContent(
+                        component = component,
+                        modifier = Modifier.fillMaxSize(),
+                    )
+                }
+            }
+        }
+
+    val detailsPane: @Composable (Child.Created<*, ArticleDetailsComponent>) -> Unit =
+        remember {
+            movableContentOf { (config, component) ->
+                saveableStateHolder.SaveableStateProvider(key = config.hashCode()) {
+                    ArticleDetailsContent(
+                        component = component,
+                        modifier = Modifier.fillMaxSize(),
+                    )
+                }
+            }
+        }
+
+    saveableStateHolder.OldDetailsKeyRemoved(selectedDetailsKey = children.detailsChild?.configuration?.hashCode())
+
     BoxWithConstraints(modifier = modifier) {
-        val model by component.models.subscribeAsState()
-        val isMultiPane = model.isMultiPane
+        when {
+            children.isMultiPane ->
+                Row(modifier = Modifier.fillMaxSize()) {
+                    Box(modifier = Modifier.fillMaxHeight().weight(0.4F)) {
+                        listPane(children.listChild)
+                    }
 
-        Row(modifier = Modifier.fillMaxSize()) {
-            ListPane(
-                stack = component.listChildStack,
-                modifier = Modifier.weight(if (isMultiPane) LIST_PANE_WEIGHT else 1F),
-            )
+                    Box(modifier = Modifier.fillMaxHeight().weight(0.6F)) {
+                        children.detailsChild?.also {
+                            detailsPane(it)
+                        }
+                    }
+                }
 
-            if (isMultiPane) {
-                Box(modifier = Modifier.weight(DETAILS_PANE_WEIGHT))
-            }
+            detailsChild != null -> detailsPane(detailsChild)
+            else -> listPane(listChild)
         }
 
-        Row(modifier = Modifier.fillMaxSize()) {
-            if (isMultiPane) {
-                Box(modifier = Modifier.weight(LIST_PANE_WEIGHT))
-            }
-
-            DetailsPane(
-                stack = component.detailsChildStack,
-                modifier = Modifier.weight(if (isMultiPane) DETAILS_PANE_WEIGHT else 1F),
-            )
-        }
-
-        val isMultiPaneRequired = this@BoxWithConstraints.maxWidth >= MULTI_PANE_WIDTH_THRESHOLD
+        val isMultiPaneRequired = this@BoxWithConstraints.maxWidth >= 800.dp
 
         DisposableEffect(isMultiPaneRequired) {
             component.setMultiPane(isMultiPaneRequired)
@@ -62,29 +86,22 @@ internal fun MultiPaneContent(component: MultiPaneComponent, modifier: Modifier 
 }
 
 @Composable
-private fun ListPane(stack: Value<ChildStack<*, ListChild>>, modifier: Modifier) {
-    Children(
-        stack = stack,
-        modifier = modifier,
-        animation = stackAnimation(fade()),
-    ) {
-        when (val child = it.instance) {
-            is ListChild.List -> ArticleListContent(component = child.component, modifier = Modifier.fillMaxSize())
-            is ListChild.None -> Box {}
-        }.let {}
-    }
-}
+private fun SaveableStateHolder.OldDetailsKeyRemoved(selectedDetailsKey: Any?) {
+    var lastDetailsKey by remember { mutableStateOf(selectedDetailsKey) }
 
-@Composable
-private fun DetailsPane(stack: Value<ChildStack<*, DetailsChild>>, modifier: Modifier) {
-    Children(
-        stack = stack,
-        modifier = modifier,
-        animation = stackAnimation(fade()),
-    ) {
-        when (val child = it.instance) {
-            is DetailsChild.None -> Box {}
-            is DetailsChild.Details -> ArticleDetailsContent(component = child.component, modifier = Modifier.fillMaxSize())
-        }.let {}
+    if (selectedDetailsKey == lastDetailsKey) {
+        return
+    }
+
+    val keyToRemove = lastDetailsKey
+    lastDetailsKey = selectedDetailsKey
+
+    if (keyToRemove == null) {
+        return
+    }
+
+    DisposableEffect(keyToRemove) {
+        removeState(key = keyToRemove)
+        onDispose {}
     }
 }
--- sample/shared/shared/src/commonMain/kotlin/com/arkivanov/sample/shared/multipane/DefaultMultiPaneComponent.kt
+++ sample/shared/shared/src/commonMain/kotlin/com/arkivanov/sample/shared/multipane/DefaultMultiPaneComponent.kt
@@ -1,109 +1,99 @@
 package com.arkivanov.sample.shared.multipane
 
+import com.arkivanov.decompose.Child
 import com.arkivanov.decompose.ComponentContext
-import com.arkivanov.decompose.router.stack.ChildStack
-import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.router.children.ChildNavState
+import com.arkivanov.decompose.router.children.ChildNavState.Status
+import com.arkivanov.decompose.router.children.NavState
+import com.arkivanov.decompose.router.children.SimpleChildNavState
+import com.arkivanov.decompose.router.children.SimpleNavigation
+import com.arkivanov.decompose.router.children.children
 import com.arkivanov.decompose.value.Value
-import com.arkivanov.decompose.value.reduce
-import com.arkivanov.essenty.backhandler.BackCallback
-import com.arkivanov.sample.shared.multipane.MultiPaneComponent.DetailsChild
-import com.arkivanov.sample.shared.multipane.MultiPaneComponent.ListChild
-import com.arkivanov.sample.shared.multipane.MultiPaneComponent.Model
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
+import com.arkivanov.sample.shared.multipane.MultiPaneComponent.Children
 import com.arkivanov.sample.shared.multipane.database.DefaultArticleDatabase
+import com.arkivanov.sample.shared.multipane.details.ArticleDetailsComponent
+import com.arkivanov.sample.shared.multipane.details.DefaultArticleDetailsComponent
+import com.arkivanov.sample.shared.multipane.list.ArticleListComponent
+import com.arkivanov.sample.shared.multipane.list.DefaultArticleListComponent
+import com.arkivanov.sample.shared.multipane.utils.disposableScope
+import com.badoo.reaktive.disposable.scope.DisposableScope
+import com.badoo.reaktive.observable.map
+import com.badoo.reaktive.observable.notNull
 import com.badoo.reaktive.subject.behavior.BehaviorSubject
 
 internal class DefaultMultiPaneComponent(
     componentContext: ComponentContext
-) : MultiPaneComponent, ComponentContext by componentContext {
+) : MultiPaneComponent, ComponentContext by componentContext, DisposableScope by componentContext.disposableScope() {
 
     private val database = DefaultArticleDatabase()
+    private val navigation = SimpleNavigation<(NavigationState) -> NavigationState>()
+    private val navState = BehaviorSubject<NavigationState?>(null)
 
-    private val _models = MutableValue(Model())
-    override val models: Value<Model> = _models
-
-    private val isDetailsToolbarVisible = BehaviorSubject(!_models.value.isMultiPane)
-    private val selectedArticleIdSubject = BehaviorSubject<Long?>(null)
-
-    private val listRouter =
-        ListRouter(
-            componentContext = this,
-            database = database,
-            selectedArticleId = selectedArticleIdSubject,
-            onArticleSelected = ::onArticleSelected
+    override val children: Value<Children> =
+        children(
+            source = navigation,
+            key = "children",
+            initialState = ::NavigationState,
+            navTransformer = { navState, event -> event(navState) },
+            stateMapper = { navState, children ->
+                @Suppress("UNCHECKED_CAST")
+                Children(
+                    isMultiPane = navState.isMultiPane,
+                    listChild = children.first { it.instance is ArticleListComponent } as Child.Created<*, ArticleListComponent>,
+                    detailsChild = children.find { it.instance is ArticleDetailsComponent } as Child.Created<*, ArticleDetailsComponent>?,
+                )
+            },
+            onStateChanged = { newState, _ -> navState.onNext(newState) },
+            childFactory = ::child,
         )
 
-    override val listChildStack: Value<ChildStack<*, ListChild>> = listRouter.stack
+    private fun child(config: Config, componentContext: ComponentContext): Any =
+        when (config) {
+            is Config.List -> listComponent(componentContext)
+            is Config.Details -> detailsComponent(config, componentContext)
+        }
 
-    private val detailsRouter =
-        DetailsRouter(
-            componentContext = this,
+    private fun listComponent(componentContext: ComponentContext): ArticleListComponent =
+        DefaultArticleListComponent(
+            componentContext = componentContext,
             database = database,
-            isToolbarVisible = isDetailsToolbarVisible,
-            onFinished = ::closeDetailsAndShowList
+            selectedArticleId = navState.notNull().map { if (it.isMultiPane) it.articleId else null },
+            onArticleSelected = { id -> navigation.navigate { it.copy(articleId = id) } },
         )
 
-    override val detailsChildStack: Value<ChildStack<*, DetailsChild>> = detailsRouter.stack
-
-    private val backCallback = BackCallback(isEnabled = false, onBack = ::closeDetailsAndShowList)
-
-    init {
-        backHandler.register(backCallback)
-
-        _models.subscribe { updateBackCallback() }
-
-        detailsRouter.stack.subscribe {
-            updateBackCallback()
-            selectedArticleIdSubject.onNext(it.active.configuration.getArticleId())
-        }
-    }
-
-    private fun updateBackCallback() {
-        backCallback.isEnabled = !isMultiPaneMode() && detailsRouter.isShown()
-    }
-
-    private fun closeDetailsAndShowList() {
-        listRouter.show()
-        detailsRouter.closeArticle()
-    }
-
-    private fun onArticleSelected(id: Long) {
-        detailsRouter.showArticle(id = id)
-
-        if (isMultiPaneMode()) {
-            listRouter.show()
-        } else {
-            listRouter.moveToBackStack()
-        }
-    }
+    private fun detailsComponent(config: Config.Details, componentContext: ComponentContext): ArticleDetailsComponent =
+        DefaultArticleDetailsComponent(
+            componentContext = componentContext,
+            database = database,
+            articleId = config.articleId,
+            isToolbarVisible = navState.notNull().map { !it.isMultiPane },
+            onFinished = { navigation.navigate { it.copy(articleId = null) } },
+        )
 
     override fun setMultiPane(isMultiPane: Boolean) {
-        _models.reduce { it.copy(isMultiPane = isMultiPane) }
-        isDetailsToolbarVisible.onNext(!isMultiPane)
-
-        if (isMultiPane) {
-            switchToMultiPane()
-        } else {
-            switchToSinglePane()
-        }
+        navigation.navigate { it.copy(isMultiPane = isMultiPane) }
     }
 
-    private fun switchToMultiPane() {
-        listRouter.show()
+    private sealed interface Config : Parcelable {
+        @Parcelize
+        object List : Config
+
+        @Parcelize
+        data class Details(val articleId: Long) : Config
     }
 
-    private fun switchToSinglePane() {
-        if (detailsRouter.isShown()) {
-            listRouter.moveToBackStack()
-        } else {
-            listRouter.show()
-        }
+    @Parcelize
+    private data class NavigationState(
+        val isMultiPane: Boolean = false,
+        val articleId: Long? = null,
+    ) : NavState<Config>, Parcelable {
+        override val children: List<ChildNavState<Config>>
+            get() =
+                listOfNotNull(
+                    SimpleChildNavState(Config.List, if (isMultiPane || (articleId == null)) Status.ACTIVE else Status.INACTIVE),
+                    if (articleId != null) SimpleChildNavState(Config.Details(articleId), Status.ACTIVE) else null,
+                )
     }
-
-    private fun isMultiPaneMode(): Boolean = _models.value.isMultiPane
-
-    private fun DetailsRouter.Config.getArticleId(): Long? =
-        when (this) {
-            is DetailsRouter.Config.None -> null
-            is DetailsRouter.Config.Details -> articleId
-        }
 }
