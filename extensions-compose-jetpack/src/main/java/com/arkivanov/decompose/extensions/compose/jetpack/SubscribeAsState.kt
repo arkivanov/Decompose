0bcd28c0504764dfc215a34288ffc6b1ee8be9c1
Arkadii Ivanov <arkann1985@gmail.com>
2023-04-14 17:47:28 +0100

    Added policy argument to subscribeAsState function

--- extensions-compose-jetpack/src/main/java/com/arkivanov/decompose/extensions/compose/jetpack/SubscribeAsState.kt
+++ extensions-compose-jetpack/src/main/java/com/arkivanov/decompose/extensions/compose/jetpack/SubscribeAsState.kt
@@ -2,14 +2,25 @@ package com.arkivanov.decompose.extensions.compose.jetpack
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SnapshotMutationPolicy
 import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.structuralEqualityPolicy
 import com.arkivanov.decompose.value.Value
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * Subscribes to the [Value] and returns Compose [State].
+ * 
+ * @param policy a [SnapshotMutationPolicy] that will be used by Compose when comparing values.
+ * Default is [structuralEqualityPolicy].
+ */
 @Composable
-fun <T : Any> Value<T>.subscribeAsState(): State<T> {
-    val state = remember(this) { mutableStateOf(value) }
+fun <T : Any> Value<T>.subscribeAsState(policy: SnapshotMutationPolicy<T> = structuralEqualityPolicy()): State<T> {
+    val state = remember(this, policy) { mutableStateOf(value, policy) }
 
     DisposableEffect(this) {
         val observer: (T) -> Unit = { state.value = it }
