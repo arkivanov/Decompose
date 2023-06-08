33a82e56823c483e5655e86e288e2cc305fb464b
Arkadii Ivanov <arkann1985@gmail.com>
2023-06-08 22:09:23 +0100

    Bumped compileSdkVersion and targetSdkVersion to 34

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.toString() == "com.arkivanov.gradle.setup") {
-                useModule("com.github.arkivanov:gradle-setup-plugin:5a89a70d63")
+                useModule("com.github.arkivanov:gradle-setup-plugin:0f3baf84e9")
             }
         }
     }
