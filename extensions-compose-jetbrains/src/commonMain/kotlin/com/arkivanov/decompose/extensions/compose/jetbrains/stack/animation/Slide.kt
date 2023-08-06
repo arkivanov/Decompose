358825c45a1af842a784ee306c938ff965ea03d6
DatLag <jeff.retz08@gmail.com>
2023-08-06 22:39:19 +0200

    added different slide directions

--- extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/Slide.kt
+++ extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/Slide.kt
@@ -8,16 +8,10 @@ import androidx.compose.ui.layout.layout
 /**
  * A simple sliding animation. Children enter from one side and exit to another side.
  */
-fun slide(animationSpec: FiniteAnimationSpec<Float> = tween()): StackAnimator =
+fun slide(
+    animationSpec: FiniteAnimationSpec<Float> = tween(),
+    slideDirection: SlideDirection = SlideDirection.EndToStart
+): StackAnimator =
     stackAnimator(animationSpec = animationSpec) { factor, _, content ->
-        content(Modifier.offsetXFactor(factor = factor))
-    }
-
-private fun Modifier.offsetXFactor(factor: Float): Modifier =
-    layout { measurable, constraints ->
-        val placeable = measurable.measure(constraints)
-
-        layout(placeable.width, placeable.height) {
-            placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt(), y = 0)
-        }
+        content(slideDirection.offset(factor))
     }
---
+++ extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/SlideDirection.kt
@@ -0,0 +1,49 @@
+package com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
+
+sealed interface SlideDirection {
+
+    fun offset(factor: Float): Modifier
+
+    object TopToBottom : SlideDirection {
+        override fun offset(factor: Float): Modifier = Modifier.layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(x = 0, y = (placeable.height.toFloat() * factor).toInt().unaryMinus())
+            }
+        }
+    }
+
+    object BottomToTop : SlideDirection {
+        override fun offset(factor: Float): Modifier = Modifier.layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(x = 0, y = (placeable.height.toFloat() * factor).toInt())
+            }
+        }
+    }
+
+    object StartToEnd : SlideDirection {
+        override fun offset(factor: Float): Modifier = Modifier.layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt().unaryMinus(), y = 0)
+            }
+        }
+    }
+
+    object EndToStart : SlideDirection {
+        override fun offset(factor: Float): Modifier = Modifier.layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+
+            layout(placeable.width, placeable.height) {
+                placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt(), y = 0)
+            }
+        }
+    }
+}
