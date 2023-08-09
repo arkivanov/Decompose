5edbd7a99374b06893089bbe867dc5edea215e43
DatLag <jeff.retz08@gmail.com>
2023-08-09 23:04:15 +0200

    simplify implementation

--- extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/Slide.kt
+++ extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/Slide.kt
@@ -2,17 +2,33 @@ package com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation
 
 import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layout
-import androidx.compose.ui.platform.LocalLayoutDirection
 
 /**
  * A simple sliding animation. Children enter from one side and exit to another side.
  */
 fun slide(
     animationSpec: FiniteAnimationSpec<Float> = tween(),
-    slideDirection: SlideDirection = SlideDirection.EndToStart
+    orientation: Orientation = Orientation.Horizontal
 ): StackAnimator =
     stackAnimator(animationSpec = animationSpec) { factor, _, content ->
-        content(slideDirection.offset(factor, LocalLayoutDirection.current))
+        content(Modifier.offset(factor = factor, orientation = orientation))
     }
+
+private fun Modifier.offset(factor: Float, orientation: Orientation): Modifier = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+    val (x, y) = when (orientation) {
+        Orientation.Horizontal -> {
+            (placeable.width.toFloat() * factor).toInt() to 0
+        }
+        Orientation.Vertical -> {
+            0 to (placeable.height.toFloat() * factor).toInt()
+        }
+    }
+
+    layout(placeable.width, placeable.height) {
+        placeable.placeRelative(x = x, y = y)
+    }
+}
