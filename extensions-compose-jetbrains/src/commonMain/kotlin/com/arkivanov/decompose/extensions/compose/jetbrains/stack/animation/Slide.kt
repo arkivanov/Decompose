da584cad3aee58567b05b8c86b176f3ff503fc3d
DatLag <jeff.retz08@gmail.com>
2023-08-11 14:01:43 +0200

    Rebase new slide method on 2.1 branch

--- extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/Slide.kt
+++ extensions-compose-jetbrains/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/jetbrains/stack/animation/Slide.kt
@@ -2,22 +2,54 @@ package com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation
 
 import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layout
 
 /**
  * A simple sliding animation. Children enter from one side and exit to another side.
  */
-fun slide(animationSpec: FiniteAnimationSpec<Float> = tween()): StackAnimator =
+@Deprecated(
+    message = "Please use the new slide StackAnimator with orientation specification",
+    replaceWith = ReplaceWith(
+        expression = "slide(animationSpec, orientation)",
+        imports = [
+            "com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.slide",
+            "androidx.compose.foundation.gestures.Orientation"
+        ]
+    ),
+    level = DeprecationLevel.HIDDEN
+)
+fun slide(
+    animationSpec: FiniteAnimationSpec<Float> = tween()
+): StackAnimator = slide(animationSpec, Orientation.Horizontal)
+
+/**
+ * A simple sliding animation. Children enter from one side and exit to another side.
+ */
+fun slide(
+    animationSpec: FiniteAnimationSpec<Float> = tween(),
+    orientation: Orientation = Orientation.Horizontal
+): StackAnimator =
     stackAnimator(animationSpec = animationSpec) { factor, _, content ->
-        content(Modifier.offsetXFactor(factor = factor))
+        content(when (orientation) {
+            Orientation.Horizontal -> Modifier.offsetXFactor(factor)
+            Orientation.Vertical -> Modifier.offsetYFactor(factor)
+        })
     }
 
-private fun Modifier.offsetXFactor(factor: Float): Modifier =
-    layout { measurable, constraints ->
-        val placeable = measurable.measure(constraints)
+private fun Modifier.offsetXFactor(factor: Float): Modifier = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
 
-        layout(placeable.width, placeable.height) {
-            placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt(), y = 0)
-        }
+    layout(placeable.width, placeable.height) {
+        placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt(), y = 0)
     }
+}
+
+private fun Modifier.offsetYFactor(factor: Float): Modifier = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    layout(placeable.width, placeable.height) {
+        placeable.placeRelative(x = 0, y = (placeable.height.toFloat() * factor).toInt())
+    }
+}
