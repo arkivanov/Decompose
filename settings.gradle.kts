b8fd35bafbc3f61670270e26efbfb68757913bf6
Arkadii Ivanov <arkann1985@gmail.com>
2024-08-08 16:38:07 +0100

    Force javaTarget 11 for all non-Android JVM targets

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.toString() == "com.arkivanov.gradle.setup") {
-                useModule("com.github.arkivanov:gradle-setup-plugin:310f7f6b99")
+                useModule("com.github.arkivanov:gradle-setup-plugin:8eb6bb973c")
             }
         }
     }
