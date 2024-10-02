96b5803f37f131c366f22ae3ef68bd699aa20bc7
Arkadii Ivanov <arkann1985@gmail.com>
2024-10-02 19:42:45 +0100

    Make default values in ChildPanelsAnimators null

--- extensions-compose-experimental/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/experimental/panels/ChildPanelsAnimators.kt
+++ extensions-compose-experimental/src/commonMain/kotlin/com/arkivanov/decompose/extensions/compose/experimental/panels/ChildPanelsAnimators.kt
@@ -2,18 +2,17 @@ package com.arkivanov.decompose.extensions.compose.experimental.panels
 
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.StackAnimator
-import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
 
 /**
  * Contains panel animators for all layout kinds.
  *
- * @param single a [StackAnimator] to be used in
+ * @param single an optional [StackAnimator] to be used in
  * [SINGLE][com.arkivanov.decompose.router.panels.ChildPanelsMode.SINGLE] mode.
- * @param dual a [Pair] of [StackAnimator] to be used in
+ * @param dual a [Pair] of optional [StackAnimator] to be used in
  * [DUAL][com.arkivanov.decompose.router.panels.ChildPanelsMode.DUAL] mode. The [first][Pair.first] animator
  * is used for the primary (first) panel, and the [second][Pair.second] animator is used for the secondary
  * (second) panel. Default value is `Pair(single, single)`.
- * @param triple a [Triple] of [StackAnimator] to be used in
+ * @param triple a [Triple] of optional [StackAnimator] to be used in
  * [TRIPLE][com.arkivanov.decompose.router.panels.ChildPanelsMode.TRIPLE] mode. The [first][Triple.first] animator
  * is used for the primary (first) panel, the [second][Triple.second] animator is used for the secondary (second)
  * panel, and the [third][Triple.third] animator is used for the tertiary (third) panel.
@@ -21,7 +20,7 @@ import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.f
  */
 @ExperimentalDecomposeApi
 class ChildPanelsAnimators(
-    val single: StackAnimator? = fade(),
-    val dual: Pair<StackAnimator?, StackAnimator?> = single to single,
+    val single: StackAnimator? = null,
+    val dual: Pair<StackAnimator?, StackAnimator?> = Pair(single, single),
     val triple: Triple<StackAnimator?, StackAnimator?, StackAnimator?> = Triple(dual.first, dual.second, dual.second),
 )
