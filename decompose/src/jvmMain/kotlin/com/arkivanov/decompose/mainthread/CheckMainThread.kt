22a836b970ed776f168c83012c9e2121d6b4670b
Arkadii Ivanov <arkann1985@gmail.com>
2026-06-27 23:06:26 +0100

    Removed deprecated code

--- decompose/src/jvmMain/kotlin/com/arkivanov/decompose/mainthread/CheckMainThread.kt
+++ decompose/src/jvmMain/kotlin/com/arkivanov/decompose/mainthread/CheckMainThread.kt
@@ -1,10 +1,10 @@
 package com.arkivanov.decompose.mainthread
 
 import com.arkivanov.decompose.DecomposeSettings
-import com.arkivanov.decompose.errorhandler.onDecomposeError
 
 internal actual fun checkMainThread() {
-    if (DecomposeSettings.settings.mainThreadCheckEnabled && mainThreadChecker?.isMainThread() == false) {
-        onDecomposeError(NotOnMainThreadException(currentThreadName = Thread.currentThread().name))
+    val settings = DecomposeSettings.settings
+    if (settings.mainThreadCheckEnabled && mainThreadChecker?.isMainThread() == false) {
+        settings.onDecomposeError(NotOnMainThreadException(currentThreadName = Thread.currentThread().name))
     }
 }
