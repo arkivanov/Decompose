06c7ad9b40f036c171644f28a59a6647d713c30b
Arkadii Ivanov <arkivanov@twitter.com>
2026-06-30 23:16:44 +0100

    Detachable child slot WIP

--- decompose/src/commonMain/kotlin/com/arkivanov/decompose/ComponentContextExt.kt
+++ decompose/src/commonMain/kotlin/com/arkivanov/decompose/ComponentContextExt.kt
@@ -1,12 +1,104 @@
 package com.arkivanov.decompose
 
+import com.arkivanov.decompose.DetachableSlotNavigation.Event
 import com.arkivanov.decompose.backhandler.child
 import com.arkivanov.decompose.instancekeeper.child
 import com.arkivanov.decompose.lifecycle.MergedLifecycle
+import com.arkivanov.decompose.router.children.NavigationSource
+import com.arkivanov.decompose.router.slot.ChildSlot
 import com.arkivanov.decompose.statekeeper.child
+import com.arkivanov.decompose.value.Value
 import com.arkivanov.essenty.backhandler.BackCallback
 import com.arkivanov.essenty.lifecycle.Lifecycle
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.arkivanov.essenty.lifecycle.doOnDestroy
+import com.arkivanov.essenty.statekeeper.SerializableContainer
+import com.arkivanov.essenty.statekeeper.StateKeeperDispatcher
+
+fun <Ctx : GenericComponentContext<Ctx>, C : Any, T : Any> Ctx.detachableChildSlot(
+    saveConfiguration: (C?) -> SerializableContainer?,
+    restoreConfiguration: (SerializableContainer) -> C?,
+    key: String = "DefaultDetachableChildSlot",
+    initialConfiguration: () -> C? = { null },
+    handleBackButton: Boolean = false,
+    childFactory: (configuration: C, Ctx) -> T,
+): Value<ChildSlot<C, T>> {
+
+}
+
+interface DetachableSlotNavigator<C : Any, T : Any> {
+
+    fun navigate(
+        transformer: (configuration: C?) -> C?,
+        onComplete: (newConfiguration: C?, oldConfiguration: C?) -> Unit,
+    )
+
+    fun attach(
+        child: DetachedChild<C, T>,
+        onComplete: (newConfiguration: C, oldConfiguration: C?) -> Unit,
+    )
+
+    fun detach(
+        predicate: (configuration: C) -> Boolean,
+        onComplete: (DetachedChild<C, T>?) -> Unit,
+    )
+}
+
+
+interface DetachableSlotNavigation<C : Any, T : Any> : DetachableSlotNavigator<C, T>, NavigationSource<Event<C, T>> {
+
+    sealed interface Event<out C : Any, out T : Any> {
+        data class Navigate<C : Any>(
+            val transformer: (configuration: C?) -> C?,
+            val onComplete: (newConfiguration: C?, oldConfiguration: C?) -> Unit = { _, _ -> },
+        ) : Event<C, Nothing>
+
+        data class Attach<C : Any, out T : Any>(
+            val child: DetachedChild<C, T>,
+            val onComplete: (newConfiguration: C, oldConfiguration: C?) -> Unit = { _, _ -> },
+        ) : Event<C, T>
+
+        data class Detach<C : Any, T : Any>(
+            val predicate: (configuration: C) -> Boolean,
+            val onComplete: (DetachedChild<C, T>?) -> Unit,
+        ) : Event<C, T>
+    }
+}
+
+internal class DefaultDetachableSlotNavigation<C : Any, T : Any> : DetachableSlotNavigation<C, T> {
+
+    private val relay = Relay<Event<C, T>>()
+
+    override fun navigate(
+        transformer: (configuration: C?) -> C?,
+        onComplete: (newConfiguration: C?, oldConfiguration: C?) -> Unit
+    ) {
+        relay.accept(Event.Navigate(transformer, onComplete))
+    }
+
+    override fun attach(
+        child: DetachedChild<C, T>,
+        onComplete: (newConfiguration: C, oldConfiguration: C?) -> Unit
+    ) {
+        relay.accept(Event.Attach(child, onComplete))
+    }
+
+    override fun detach(
+        predicate: (configuration: C) -> Boolean,
+        onComplete: (DetachedChild<C, T>?) -> Unit
+    ) {
+        relay.accept(Event.Detach(predicate, onComplete))
+    }
+
+    override fun subscribe(observer: (Event<C, T>) -> Unit): Cancellation =
+        relay.subscribe(observer)
+}
+
+
+interface DetachedChild<out C : Any, out T : Any> {
+    val child: ChildSlot<C, T>
+    val lifecycle: LifecycleRegistry
+}
 
 /**
  * Creates a new instance of child [ComponentContext] of type [Ctx] and attaches it to the parent (`this`) component context.
