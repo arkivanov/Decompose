1bfd56905f67c6adfbe3261a3e371e465703628d
Arkadii Ivanov <arkann1985@gmail.com>
2026-07-03 10:38:21 +0100

    Migrated samples from Children to ChildStack

--- sample/shared/compose/src/commonMain/kotlin/com/arkivanov/sample/shared/tabs/TabsContent.kt
+++ sample/shared/compose/src/commonMain/kotlin/com/arkivanov/sample/shared/tabs/TabsContent.kt
@@ -22,9 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.stack.Children
-import com.arkivanov.decompose.extensions.compose.stack.animation.fade
-import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
+import com.arkivanov.decompose.extensions.compose.experimental.stack.ChildStack
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.stackAnimation
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import com.arkivanov.sample.shared.SwipeUp
 import com.arkivanov.sample.shared.cards.CardsContent
@@ -49,7 +49,7 @@ internal fun TabsContent(
 
 @Composable
 private fun Children(component: TabsComponent, modifier: Modifier = Modifier) {
-    Children(
+    ChildStack(
         stack = component.stack,
         modifier = modifier,
         animation = stackAnimation(fade()),
